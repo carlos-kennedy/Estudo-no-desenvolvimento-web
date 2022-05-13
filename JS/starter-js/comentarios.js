9bc3154f53cbe7efc43a3450fcd159ed5366696b
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-13 15:49:51 -0300

    main

---
+++ JS/starter-js/comentarios.js
@@ -0,0 +1,8 @@
+// Os códigos abaixo escrevem algo muito importante no console do devtools. ;D
+
+console.log('Você consegue me vê aqui dentro?')
+
+console.log('Se sim, gostaria de lhe dizer que para comentar algo utilize o "//" ou o "/*" para mais linhas, tá bom?')
+
+
+/*Você conseguiu vê os códigos?*/
