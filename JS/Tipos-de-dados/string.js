fa72606909de85ffbfaa96e7d6118e44318cffab
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-13 16:21:18 -0300

    main

---
+++ JS/Tipos-de-dados/string.js
@@ -0,0 +1,20 @@
+// String
+
+// Cadeia de caracteres do tipo texto
+
+// ""   // aspas duplas
+// ''   // aspas simples
+// ´´  // template literals ou template strings 
+// (Pode ser utilzado com aspas duplas e aspas simples)
+// (Pode ser utilizado como multilinha )
+// (Pode ser utilizado para expressões)
+console.log('Kennedy')
+
+console.log("Me responda uma coisa")
+
+console.log(`Eu estou 
+querendo saber o
+resultado de 1+1,
+qual é o resultado?`)
+
+console.log(`${1 + 1}`)
