0c41a36ac3b85316158e46ae7550a7f761ee248f
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-13 16:36:45 -0300

    main

--- JS/Tipos-de-dados/number.js
+++ JS/Tipos-de-dados/number.js
@@ -1,7 +1,19 @@
 // Number
-        // Números
+// Números
 
-        // 33   - Inteiro
-        // 12.5 - Real
-        // NaN - Not a Number
-        // Infinity - Inifinito
+// 33   - Inteiro
+// 12.5 - Real (Float)
+// NaN - Not a Number
+// Infinity - Inifinito
+
+
+console.log(33, -33, 20)
+console.log(3.3, 3.1, -2.1)
+
+console.log(33 + 3.3, 33 - 3.1, 20 - 2.1)
+
+console.log("Sou um número, confia em mim" * 33)
+
+console.log("Sou infinito" === Infinity)
+
+console.log(Infinity)
