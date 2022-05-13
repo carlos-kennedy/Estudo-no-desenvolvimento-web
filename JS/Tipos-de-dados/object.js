4ed15f69666d5be96f8002d7865e40df1c38472c
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-13 18:13:20 -0300

    main

---
+++ JS/Tipos-de-dados/object.js
@@ -0,0 +1,15 @@
+// Object
+// Objeto
+// Propriedades / Atributos
+// Funcionalidades / Métodos
+
+// { Propriedade: "valor"}
+
+console.log({
+    name: "Kennedy",
+    idade: 21,
+    andar: function () {
+        console.log('andar')
+    }
+})
+// A estrutura básica de um objeto
