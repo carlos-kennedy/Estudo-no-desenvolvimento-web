f4cd91b111427a6d970979051df6ede447b50d10
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-13 18:20:23 -0300

    main

---
+++ JS/Tipos-de-dados/array.js
@@ -0,0 +1,25 @@
+// Array (vetores)
+// Uma lista
+// Agrupamento de dados
+
+// ["Kennedy", 21]
+
+
+console.log([
+    'Carlos',
+    21])
+
+// Exemplo com concatenação
+// console.log([
+//     'Leite ' + 3,
+//     'Ovos ' + 12,
+//     'Carne ' + 1,
+//     'Manteiga ' + 2
+// ])
+
+console.log([
+    'Ovos',
+    'Leite',
+    'Carne',
+    'Manteiga'
+])
