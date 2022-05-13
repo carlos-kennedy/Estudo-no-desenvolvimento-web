622b308d97582d44fe69c95898d59c050ee99796
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-13 15:15:59 -0300

    main

---
+++ JS/sintax.js
@@ -0,0 +1,9 @@
+// # A importancia da Sintaxe
+
+// Forma de escrever seu texto de uma maneira correta
+
+// Toda linguagem possui 
+// Uma boa comunicação necessita de uma boa sintaxe
+// 82% de erros para iniciantes sempre está relacionada a sintaxe
+
+console.log("Seja Bem vindo(a) ao meu mundo Starter")
