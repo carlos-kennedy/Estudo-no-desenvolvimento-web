8b723aa2fc69defb9b5733b2e3a0bad8e2da7a84
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-13 15:09:20 -0300

    main

--- JS/introducao.js
+++ JS/introducao.js
@@ -1,1 +1,42 @@
 // Introdução
+
+// #JavaScript core
+
+// Linguagem de programação que roda nos navegadores do usuário (front-end)
+    // Se você clicar em qualquer botão e aparecer uma janela, isso é javascript
+    // Alteração do site ou aplicativo, dependendo da interação do usuário
+
+    // Roda também no  computador (back-end)
+
+ // #O que podemos fazer?
+
+    // Podemos criar diversas interações com aplicações web, mobile (react native), desktop (Electron)
+    // Empresas famosas que usam
+        // Facebook (Instagram, whatsapp)
+        // Twitter
+        // Google
+        // Uber
+        // Netflix
+        // TikTok
+        // ...
+
+    // 99,99% dos sites na web utilizam o JavaScript
+    // Linguagem obrigatória para quem programa o Front-end web
+
+ // Evolução
+    // A comunidade cresce cada vez mais e a linguagem está sempre evoluindo
+
+    // O que vamos ver no curso?
+    // Tipos de dados
+    // Variáveis
+    // Funções
+    // Condicionais
+    // Estruturas de repetição
+    // ...
+
+ // Como vai funcionar a dinâmica do curso?
+    // Treine bastante, tenha uma boa consistência nos seus estudos.
+ 
+    // Conceitos e fundamentos
+    // Prática
+    // Revisão
