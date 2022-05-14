6998af0353d3d2453be7b4d60bca97bab5deefff
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-13 23:39:28 -0300

    main

---
+++ JS/variáveis/variaveis-nomes.js
@@ -0,0 +1,38 @@
+// ##Para criar nomes
+
+// JS é case-sensitive (sensível ao caso)
+// JS aceita a cadeia de caracteres Unicode (pode colocar aspas, acentuações ...)
+
+// Posso:
+    // Iniciar com estes caracteres especias: $ _
+    // Iniciar com letras 
+    // Colocar acentos
+    // Letras maiúsculsa e minúsculas fazem diferença
+
+ // Exemplos que podem ser aceitos:
+// let Kennedy = 'Kennedy'
+// let kennedy ='kennedy'
+// let Kênnedy = 'Kênnedy'
+// let kênnedy = 'kênnedy'
+// let $varName = 'true'
+// let $_io = "false"
+// let _varNumb = 21
+
+// console.log('São permitidos a serem usados para nomes de declarações de variáveis: ', Kennedy, kennedy, Kênnedy, kênnedy, $varName, $_io, _varNumb)
+
+// Não posso:
+    // Iniciar com números
+    // Colocar espaços vazios no nome
+
+// Exemplos que não são aceitos:
+// let 230 = "230"
+// let sou um nome vazio = "sou um nome vazio" 
+
+// console.log('Estes não podem ser aceitos como declaração de variável')
+
+// Ideal:
+    // Criar nomes que fazem sentido
+    // Que explique o que a variável é ou faz 
+    // cameCase
+    // snake_case
+    // Escrever em inglês
