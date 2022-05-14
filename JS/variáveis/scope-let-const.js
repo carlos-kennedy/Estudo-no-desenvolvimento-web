bc9485f273b73bf497a29840b290880f29a59df3
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-13 22:54:37 -0300

    main

--- JS/variáveis/scope-let-const.js
+++ JS/variáveis/scope-let-const.js
@@ -19,9 +19,18 @@
 
 // console.log('> existe y antes do bloco? ', y)
 
+// let declarado em escopo global
+let y = 1
+
 {
-    let y = 0
+    // let y = 0
+    // console.log('>existe y? ', y)
+
+    y = 0
     console.log('>existe y? ', y)
+
+    // o y poderá ter sua troca de valor dentro do escopo sem uma "palavra chave" (let), pois ele procurará um outro y em um outro escopo.
 }
 
-// console.log('> existe y depois do bloco? ', y)
+// Só não terá erro se for definido um let no scopo global
+console.log('> existe y depois do bloco? ', y) 
