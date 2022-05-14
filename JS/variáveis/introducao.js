0cfe5567a99ac2fe92c04f9671b366df014aaaf2
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-13 21:40:28 -0300

    main

---
+++ JS/variáveis/introducao.js
@@ -0,0 +1,9 @@
+// # Variáveis
+
+// Nomes simbólicos para receber um valor
+// Atalhos de códigos
+// Identificadores
+// Existem 3 palavras reservadas para criar uma váriavel
+    // var
+    // let
+    // const
