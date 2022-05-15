85aa27546413e3180883a952bb650de72538040e
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-15 11:18:37 -0300

    main

---
+++ JS/variáveis/Revisões/arrays.js
@@ -0,0 +1,14 @@
+// Arrays
+
+const animals = ['Lion', 'Tiger', 'Cat', 'Dog', 'Monkey',
+    // Adicionando um objeto também
+    {
+        animal: 'Cat',
+        namePet: 'Charllote',
+        age: 7, 
+        weight: 20.3       
+    }
+]
+// 'length' é usado para saber o total de dados de dentro do Array
+// Se não houver um dado em alguma posição no qual você colocou em [] o resultado será 'undefined'
+console.log(animals[2], animals.length, animals[5])
---
+++ JS/variáveis/Revisões/object.js
@@ -0,0 +1,11 @@
+// Object
+
+
+const person = {
+    name: 'Charllote',
+    age: 23,
+    weight: 35.3,
+    isAdmin: true
+}
+
+console.log(`A ${person.name} tem ${person.age} anos `)
