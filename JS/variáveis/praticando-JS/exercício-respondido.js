5ef69f3e9559ec1f0a23b5b2ac9065d9b4080c5d
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-15 15:06:09 -0300

    main

---
+++ JS/variáveis/praticando-JS/exercício-respondido.js
@@ -0,0 +1,59 @@
+//  1. Declare uma variável com nome weight
+// 1.1 Atribua um valor a ela (opcional)
+
+// 2. Diga que tipo de dado é essa variável
+
+
+
+// 3. Declare uma variável e atribua valores para cada um dos dados
+// -name: String
+// -age: Number (integer)
+// -stars: Number (float)
+// -isSubscribed: Boolean
+
+
+// 4. A variável students abaixo é de que tipo de dado?
+
+
+
+//  4.1. Atribua a ela as mesmas propriedades e valores do exercício 3
+
+
+
+//   4.2. Mostre no console a seguinte mensagem:
+//  <name> de idade <age> pesa <weight> kg.
+//   Atenção, substitua <name> <age> e <weight> pelos valores de cada propriedade do objeto.
+
+
+
+// console.log(`O ${student.name} de idade ${student.age} pesa ${student.weight} kg`)
+
+
+
+// 5. Declare uma variável do tipo Array, de nome students e atribua a ela nenhum valor, ou seja, somente o Array vazio
+
+
+
+// 6. Reatribua valor para a variável acima, colocando dentro dela o objeto da questão 4. (Não copiar e colar o objeto mas usar o objeto criado e inserir ele no Array)
+
+
+
+// 7. coloque no console o valor da posição zero do Array acima
+
+
+
+// 8. Crie um novo objeto colocando ele na posição 1 do Array students
+
+
+
+// Outra forma de adicionar uma nova posição de um objeto no Array
+
+
+
+// 9. Sem rodar o código abaixo responda qual é a resposta do código abaixo e por que? Após sua resposta, rode o código e veja se você acertou.
+
+/*
+ console.log(a)
+    var a = 1
+    // Undefined, pois o var é uma variável flexível e possui um tratamento único de hoisting (elevação) o valor fica em baixo enquanto a declaração fica em cima do console.log
+*/
