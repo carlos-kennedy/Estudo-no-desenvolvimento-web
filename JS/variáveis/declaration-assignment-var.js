5e4665c2b1cb30efd5d3cde5ddafaac4de37bd62
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-15 10:51:16 -0300

    main

---
+++ JS/variáveis/declaration-assignment-var.js
@@ -0,0 +1,27 @@
+// Revisando com Variávei e Tipos de dados
+
+// Declaração or declaration
+var name
+
+// Assignment or atribuição de valores
+name = 'Kennedy'
+
+// Que tipo de dado foi colocado na varíável?
+// console.log(typeof name)
+
+
+// Você pode agrupar as declarações com o ','
+// Sem agrupamento de declarações
+// let age = 21
+// let isHuman = true
+
+// Com agrupamento de declarações
+let age, isHuman
+age = 21
+isHuman = true
+
+// Multiplos argumentos na função
+// console.log('As entradas de dados são: ', name, age, isHuman)
+
+// Escrita de texto com multiplos argumentos com variáveis
+console.log ('O ' + name + ' tem ' + age + ' anos')
