fe6e5ca32ab69b6c31a0f7adbda41fed64f21dae
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-15 11:38:12 -0300

    main

--- JS/variáveis/Revisões/arrays.js
+++ JS/variáveis/Revisões/arrays.js
@@ -5,10 +5,13 @@ const animals = ['Lion', 'Tiger', 'Cat', 'Dog', 'Monkey',
     {
         animal: 'Cat',
         namePet: 'Charllote',
-        age: 7, 
-        weight: 20.3       
+        age: 7,
+        weight: 20.3
     }
 ]
 // 'length' é usado para saber o total de dados de dentro do Array
+
 // Se não houver um dado em alguma posição no qual você colocou em [] o resultado será 'undefined'
-console.log(animals[2], animals.length, animals[5])
+console.log(animals[2], animals.length,
+    // Object
+    animals[5], animals[5].namePet)
---
+++ JS/variáveis/praticando-JS/exercício.js
@@ -0,0 +1,7 @@
+//  1. Declare uma variável com nome weight
+
+// 2. Diga que tipo de dado é essa variável
+
+
+let weight = 65.5
+console.log('O peso é: ' + weight + ' e o seu tipo de dado é: '+ typeof weight)
