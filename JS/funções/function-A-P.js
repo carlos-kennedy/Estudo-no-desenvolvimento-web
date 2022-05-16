0372aba6abff91fb9c2ce2237e83b0d14c8ea776
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-16 14:16:58 -0300

    main

---
+++ JS/funções/function-A-P.js
@@ -0,0 +1,30 @@
+// Pra que serve functions?
+// Agrupamento de códigos
+// Reutilização de código quantas vezes quiser
+// Entregar um significado na declaração do bloco no código
+
+// Exemplo:
+// Gostaria de criar uma aplicação de frases motivacionais
+
+// console.log('Nunca fale para uma pessoa o que está fazendo ou o que vai fazer da sua vida futuramente, a inveja vem de onde a gente menos espera')
+
+// console.log('Pratique e estude o suficiente para que não precise depender de alguém.')
+
+// console.log('Foque mais em você atualmente e futuramente, saúde mental é algo sagrado, e devemos respeitar isso.')
+
+// Anatomia function
+
+// Declaration - declaração da função or function statement
+function createPhrases() {
+    console.log('Nunca fale para uma pessoa o que está fazendo ou o que vai fazer da sua vida futuramente, a inveja vem de onde a gente menos espera')
+
+    console.log('Pratique e estude o suficiente para que não precise depender de alguém.')
+
+    console.log('Foque mais em você atualmente e futuramente, saúde mental é algo sagrado, e devemos respeitar isso.')
+}
+
+// Executar a função (rodar, chamar, invocar ...) or execute function (run, call, invoke)
+
+createPhrases()
+
+console.log('Fim do meu programa')
