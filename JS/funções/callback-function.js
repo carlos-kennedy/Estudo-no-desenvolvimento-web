e53c12cb576d4e7f10431adc1c2bb3581867ee90
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-17 14:44:47 -0300

    main

---
+++ JS/funções/callback-function.js
@@ -0,0 +1,20 @@
+// callback function
+// Vai chamar uma função e vai chamar outra de volta
+
+
+// Guardou na memória
+function sayMyName(name) {
+    console.log('Antes de executar a função de callback')
+
+    // Esta é a função que foi trazida de volta
+    name()
+
+    console.log('Depois de executar a função de callback')
+}
+
+// Ao executar enviou de volta para a memória
+sayMyName(
+    () => {
+        console.log('Estou aprendendo callback')
+    }
+)
