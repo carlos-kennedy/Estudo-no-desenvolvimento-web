cceaee0a7018eee1e3f0178640105dafa047f902
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-16 22:00:51 -0300

    main

---
+++ JS/funções/function-plus-explained.js
@@ -0,0 +1,10 @@
+// Function de uma maneira lúdica
+// Pense que a function é um liquidifacor
+
+function makeVitamin(fruit1, fruit2, fruit3, liquids) {
+    return 'Fiz uma vitamina de: ' + fruit1 + fruit2 + fruit3 + liquids
+}
+
+const copo = makeVitamin(' Banana ', ' Maçã ', ' Morango ', ' Leite ')
+
+console.log(copo)
