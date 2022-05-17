90299f0755b3dd8ea6d10e3acb836db83849c8eb
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-17 17:47:18 -0300

    main

--- JS/funções/function-scope.js
+++ JS/funções/function-scope.js
@@ -5,5 +5,8 @@ function createThink(subject) {
     subject = 'Make twerk'
     return subject
 }
+// escopo global
 console.log(subject)
+
+// escopo local function
 console.log(createThink(subject))
