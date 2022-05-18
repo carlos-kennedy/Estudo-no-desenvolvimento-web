f9c74aff846eaae1b9ffa0d2388abe916c2e2e70
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-17 21:19:39 -0300

    main

---
+++ JS/manipulação-de-dados/type-conversion.js
@@ -0,0 +1,8 @@
+// Type conversion (type casting) vs Type coersion
+// Alteração de um tipo de dado para outro tipo
+
+// Exemplo de coersion (forçar troca)
+console.log('9' + 5) //O programa fez uma troca forçada para que não mostre nenhum erro e concatenando o number com uma string
+
+// Exemplo de Type conversion
+console.log(Number('9') + 5) //Faz com altere o tipo de dado e convertendo para outro tipo (String para number)
