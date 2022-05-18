bf5790b1222a957628233ee0385566476c5e6c04
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-17 21:36:56 -0300

    main

--- JS/manipulação-de-dados/manipulando-strings-e-numbers.js
+++ JS/manipulação-de-dados/manipulando-strings-e-numbers.js
@@ -1,17 +1,24 @@
 // Manipulando strings e numbers
 // Transformar strings para numbers e numbers para strings
 
-let string = '20'
+// let string = '20'
 // nunca esqueça coloque em maiúsculo o inicio das declarações das funções
-console.log(Number(string))
+// console.log(Number(string))
 
-let number = 32
+// let number = 32
 // Não é obrigatório adiciona-los no console.log, demonstração usada apenas para mostrar na tela
-console.log(String(number) + 20)
+// console.log(String(number) + 20)
 
 // Contar quantos caracteres tem em uma palavra e quantos dígitos tem um número
 
-let word = 'Kennedy'
-console.log(word.length)
-let anyNumber = 2983132
-console.log(String(anyNumber).length)
+// let word = 'Kennedy'
+// console.log(word.length)
+// let anyNumber = 2983132
+// console.log(String(anyNumber).length)
+
+// Transformando um número quebrado com 2 casa decimais e trocar pontos por vírgula
+
+let number = 231.120
+// O que estiver atrelado a o number podem ser chamado de metodos também.
+// O replace funciona assim, "replace(1 opção, 2 opção)" ele trocará a primeira opção pela segunda
+console.log(number.toFixed(2).replace(".", ","))
