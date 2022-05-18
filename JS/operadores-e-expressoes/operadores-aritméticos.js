60ef433b074a964fc19318f7b40ebcb2c95c8af1
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-18 14:41:04 -0300

    main

---
+++ JS/operadores-e-expressoes/operadores-aritméticos.js
@@ -0,0 +1,53 @@
+// Operadores aritméticos
+// Multiplicação *
+console.log((3 * 20, 30.4 * 30.1))
+
+// Divisão /
+console.log(3 / 29)
+
+// Soma +
+console.log(3 + 2)
+
+// Subtração -
+console.log(3 - 2)
+
+// Resto da divisão %
+let remainder = 5
+console.log(remainder % 3)
+
+// Incremento++ (após)
+// Incrementar quer dizer adicionar mais 1 número
+let increment = 0
+increment++
+increment++
+increment++
+console.log(++increment)
+
+//++ Incremento  (antes)
+// Incrementar quer dizer adicionar mais 1 número
+// let increment = 0
+// ++increment
+// console.log(++increment)
+// console.log(increment)
+
+// Decremento -- (após)
+// O decrement subtrai 1 número
+let decrement = 0
+decrement--
+decrement--
+decrement--
+decrement--
+console.log(decrement)
+
+// Decremento -- (antes)
+// let decrement = 0
+// decrement--
+// decrement--
+// decrement--
+// decrement--
+// console.log(decrement)
+
+
+
+// Exponencial**
+console.log(3 ** 3)
