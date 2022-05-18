4dd92d026f7206faf8584011ba0ea953ecee430d
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-18 15:38:36 -0300

    main

--- JS/operadores-e-expressoes/operadores-de-comparacao.js
+++ JS/operadores-e-expressoes/operadores-de-comparacao.js
@@ -14,12 +14,36 @@ let two = 2
 // console.log(one != 1)
 // console.log(one != "1")
 
-// === Estritamente igual a
+// === estritamente igual a
+// Faz com que compare o seu valor e o seu tipo
 // console.log (one === '1')
 // console.log (one === 1)
 
-// !== Estritamente diferente a
+// !== estritamente diferente a
+// Faz com que compare o seu valor e o seu tipo
 // console.log(one !== two)
 // console.log( one !== 2)
 // console.log( two !== 1)
 // console.log( two !== 2)
+
+// > maior a
+// console.log(one > two)
+// console.log(two > one)
+// console.log(one > 0)
+
+// < menor a
+// console.log(one < two)
+// console.log(two < one)
+// console.log(one < 2)
+
+// >= maior ou igual a
+// console.log(one >= two)
+// console.log(two >= one)
+// console.log(one >= -1)
+// console.log(two >= 3)
+
+// <= menor ou igual a
+// console.log(one <= two)
+// console.log(two <= one)
+// console.log(one <= 3)
+// console.log(two <= 2)
