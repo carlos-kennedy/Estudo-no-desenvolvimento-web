85bc95a1bafde925fd55504cdbcd83fea0e471a0
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-18 13:50:07 -0300

    main

--- JS/operadores-e-expressoes/introducao.js
+++ JS/operadores-e-expressoes/introducao.js
@@ -8,17 +8,17 @@
 
 // Expression são qualquer coisa em linha que resolvem alguma coisa
 
-// Toda expression no js pode ou não terminar com ;
-let number = 20
+// Toda expression no js pode ou não terminar com ; pois é bem facultativo no js
+let number = 1
 
 // Um dos poucos casos que você poderá usar o ;
 // Exemplo
-let otherNumber = 2;
+// let otherNumber = 2;
 
 // Em uma expressão auto executável com o ;
-(function () {
-    console.log('Oie')
-})()
+// (function () {
+//     console.log('Oie')
+// })()
 
 // Como o JS entende isso por debaixo dos panos
 // let otherNumber = 2 (function () {
@@ -26,3 +26,9 @@ let otherNumber = 2;
 // })()
 
 // Aí esta dizendo que ele vai executar o 2 passar uma função como argumento e então receber uma outra função
+
+console.log(number + 1) //Binary por ter dois valores para que o operador esteja entre eles.
+
+console.log(++number) //Unary por ter apenas um valor e receber algum tipo de operador como incremento tipo o "typeof" que é um operador unary
+
+console.log(false ? 'Lindo' : 'Lindo demais') //Ternary possui valores com boolean com opções de condiçãos true or false. No exemplo acima você utiliza duas opções de se e senão nos : , se o for true dará a primeira opção se for false entrará na segunda opção
