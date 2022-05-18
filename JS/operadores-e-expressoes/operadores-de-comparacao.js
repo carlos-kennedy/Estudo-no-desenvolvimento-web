8449baf399fe675cb23f95bf76502796f588f841
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-18 15:25:47 -0300

    main

--- JS/operadores-e-expressoes/operadores-de-comparacao.js
+++ JS/operadores-e-expressoes/operadores-de-comparacao.js
@@ -13,3 +13,13 @@ let two = 2
 // console.log(one != two)
 // console.log(one != 1)
 // console.log(one != "1")
+
+// === Estritamente igual a
+// console.log (one === '1')
+// console.log (one === 1)
+
+// !== Estritamente diferente a
+// console.log(one !== two)
+// console.log( one !== 2)
+// console.log( two !== 1)
+// console.log( two !== 2)
