f4fc7723c064d69963fc81730d712e0657fa7ac6
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-17 21:12:01 -0300

    main

--- JS/manipulação-de-dados/prototype.js
+++ JS/manipulação-de-dados/prototype.js
@@ -2,3 +2,13 @@
     // Prototype-based language
     // Prototype chain
     // ____proto____
+
+// É uma funcionalidade no próprio JS.
+
+// Ele é englobado em um objeto
+
+// Ele traz outros protótipos junto com outras propriedades em protótipo, porém as que ficão mais acima são as mais puras de se usar.
+
+// Com ele nós podemos ver qual propriedade pode ser manipulada.
+
+// Ele pode ser acessado como um objeto no ' . '  em um console ou no DevTools 
