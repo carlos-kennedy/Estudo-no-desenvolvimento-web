5b7186b197689fd7a7fc590c558186e422334500
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-18 19:39:34 -0300

    main

---
+++ JS/operadores-e-expressoes/operadores-de-atribuicao.js
@@ -0,0 +1,60 @@
+// Operadores de atribuição (Assignment)
+
+let x
+let y
+
+// console.log(x, y)
+
+// Assignment
+x = 1
+y = 1
+
+// Addition assignment
+// Faz com que adicione mais pelo valor dado
+// Sem o operador de atribuição 
+// x = x + 2
+// y = y + 2
+x += 2
+y += 2
+
+// Subtraction assignment
+// Faz com que subtraia pelo valor dado
+// Sem o operador de atribuição 
+// x = x - 3
+// x = x - 2
+x -= 3
+y -= 2
+
+// Multiplication assignment
+// Faz com que multiplique pelo valor dado
+// Sem o operador de atribuição 
+// x = x * 2
+// y = y * 3
+x *= 2
+y *= 3
+
+// Division assignment
+// Faz com que divida pelo valor dado 
+// Sem o operador de atribuição 
+// y = y / 2
+// x = x / 2
+x /= 2
+y /= 2
+
+// Remainder
+// Faz com que devolva o resto divido pelo valor dado 
+// Sem o operador de atribuição 
+// x = x % 3
+// y = y % 5
+x %= 3
+y %= 5
+
+// exponetiation
+// Faz com que faça a exponenciação pelo valor dado
+// Sem o operador de atribuição
+// x = x ** 3
+// y = y ** 5
+x **= 2
+y **= 5
+
+console.log(x, y)
