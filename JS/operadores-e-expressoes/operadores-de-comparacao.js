75ee31455f559ae5d798d71a6bdfc277bbb52aec
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-18 15:19:03 -0300

    main

---
+++ JS/operadores-e-expressoes/operadores-de-comparacao.js
@@ -0,0 +1,15 @@
+// Operadores de comparação
+
+// Irá comparar valores com Boolean que retornará uma resposta á comparação
+
+let one = 1
+let two = 2
+
+// == igual a
+// console.log (one == 2)
+// console.log (two == 1)
+
+// != diferente a
+// console.log(one != two)
+// console.log(one != 1)
+// console.log(one != "1")
