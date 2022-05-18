7d86e2c143acea833c11112f266936bdd66ef747
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-18 19:46:33 -0300

    main

---
+++ JS/operadores-e-expressoes/operadores-lógicos.js
@@ -0,0 +1,18 @@
+// Operadores lógicos (logical operators)
+
+// - 2 valores booleanos, quando verificados, resultará em verdadeiro ou falso
+
+let pao = true
+let queijo = false
+let manteiga = true
+
+// AND && 
+console.log(pao && queijo && manteiga)
+
+
+// OR ||
+console.log(pao || queijo || manteiga)
+
+
+// NOT !
+console.log(!pao, !queijo, !manteiga)
