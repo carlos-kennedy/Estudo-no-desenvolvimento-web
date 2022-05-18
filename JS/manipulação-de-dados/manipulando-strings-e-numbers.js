bb4487558a3662e8a90a29d5455e7c1c4aca3d5f
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-17 22:08:41 -0300

    main

--- JS/manipulação-de-dados/manipulando-strings-e-numbers.js
+++ JS/manipulação-de-dados/manipulando-strings-e-numbers.js
@@ -11,24 +11,42 @@
 
 // Contar quantos caracteres tem em uma palavra e quantos dígitos tem um número
 
-// let word = 'Kennedy'
-// console.log(word.length)
-// let anyNumber = 2983132
-// console.log(String(anyNumber).length)
+let Word = 'Kennedy'
+console.log(word.length)
+
+let anyNumber = 2983132
+console.log(String(anyNumber).length)
 
 // Transformando um número quebrado com 2 casa decimais e trocar pontos por vírgula
 
-// let number = 231.120
+let number = 231.120
 // O que estiver atrelado a o number podem ser chamado de metodos também.
 // O replace funciona assim, "replace(1 opção, 2 opção)" ele trocará a primeira opção pela segunda
-// console.log(number.toFixed(2).replace(".", ","))
+console.log(number.toFixed(2).replace(".", ","))
 
 // Transformando letras minúsculas e maiúsculas. faça o contrário disso também.
 
-// let word = 'Olá, como estou formatado?'
-// console.log(word.toUpperCase())
-// console.log(word.toLowerCase())
+let anyWord = 'Olá, como estou formatado?'
+
+console.log(anyWord.toUpperCase())
+
+console.log(anyWord.toLowerCase())
 
 // Verificando se na frase abaixo exite a palvra "Amor"
-let phraseWithLove = 'Tenho muito por estar aprendendo algo no qual me identifico!'
-console.log(phraseWithLove.includes('amor'))
+// Este metodo é case sensitive a palavra que ele encontrar tem que ser idêntica a ela.
+
+// let phraseWithLove = 'Tenho muito por estar aprendendo algo no qual me identifico!'
+// console.log(phraseWithLove.includes('Amor'))
+
+// Separe um texto que contém espaços, em um novo Array onde todo o texto é uma posição do Array. Depois disso, transforme o array em um texto e onde eram os espaços adicione _
+
+let phrases = 'Eu quero viver o Amor e a liberdade!'
+
+// O split faz com que separe as palavras com o argumento dentro dele, no caso foi o espaço
+
+// O join faz com que junte o que tiver no argumento dentro dele
+let anyArray = phrases.split(" ")
+console.log(anyArray)
+
+let phraseWithUnderscore = anyArray.join("_")
+console.log(phraseWithUnderscore)
