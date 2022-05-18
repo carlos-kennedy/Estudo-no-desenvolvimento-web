44db49fbfd015aa91140f6d5ec0982b968c02e51
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-17 21:31:21 -0300

    main

--- JS/manipulação-de-dados/manipulando-strings-e-numbers.js
+++ JS/manipulação-de-dados/manipulando-strings-e-numbers.js
@@ -1,2 +1,17 @@
 // Manipulando strings e numbers
 // Transformar strings para numbers e numbers para strings
+
+let string = '20'
+// nunca esqueça coloque em maiúsculo o inicio das declarações das funções
+console.log(Number(string))
+
+let number = 32
+// Não é obrigatório adiciona-los no console.log, demonstração usada apenas para mostrar na tela
+console.log(String(number) + 20)
+
+// Contar quantos caracteres tem em uma palavra e quantos dígitos tem um número
+
+let name = 'Kennedy'
+console.log(name.length)
+let anyNumber = 2983132
+console.log(String(anyNumber).length)
