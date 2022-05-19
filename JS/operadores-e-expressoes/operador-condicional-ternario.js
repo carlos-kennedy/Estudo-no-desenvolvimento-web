4ac5403b6b3c1f4675b48d765f3d39651e4c304e
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-18 22:09:24 -0300

    main

--- JS/operadores-e-expressoes/operador-condicional-ternario.js
+++ JS/operadores-e-expressoes/operador-condicional-ternario.js
@@ -1,6 +1,9 @@
 // Operador condicional ternário
+
 // Dependendo da condição nos recebemos valores diferentes
+
 // Condição então valor 1 se não valor 2:
+
 // condition ? value1 : value2
 
 // Exemplos
@@ -13,12 +16,14 @@ let cafe = true
 // const niceBreakfeast = pao && manteiga && cafe ? 'Café da manhã top :)' : 'Falto alguma coisa, e eu não sei o que é :('
 
 const niceBreakfeastOrBad = pao || manteiga || cafe ? 'Café da manhã top :)' : 'Falto alguma coisa, e eu não sei o que é :('
+// Qualquer coisa pode entrar na condicional ternário. functions arrays objects ...
 
 console.log(niceBreakfeastOrBad)
 
 // Maior de 18
 
 let age = 18
-const canDrive = age >= 18 ? 'Pode dirigir' : 'Não pode dirigir'
+const canDrive = age >= 18 ? "Can drive" : "Can't  drive"
+// Qualquer coisa pode entrar na condicional ternário. functions arrays objects ...
 
 console.log(canDrive)
