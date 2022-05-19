7a1c4fee5b9b72c6566dffc10e19579035ab1661
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-18 22:43:17 -0300

    main

--- JS/operadores-e-expressoes/precendencia-operadores.js
+++ JS/operadores-e-expressoes/precendencia-operadores.js
@@ -1,5 +1,5 @@
 // Operator precedence
-    // Precedencia de operadores
+// Precedencia de operadores
 
 // Grouping                                                              ()
 // Negação e Incremento                                     ! ++ --
@@ -12,3 +12,11 @@
 // Condicional                                                         ?:
 // Assignment (Atribuição)                                = += -= *=
 
+console.log(2 + 2 * 10 + 30 * 3)
+// console.log(3 > 2 > 1)
+// Pode debaixo dos panos esta expressão no JS
+console.log(3 > 2 > 1)
+//console.log(true===1) false 
+//console.log(true==1) true
+// O true é transformado em 1
+// O false é transformado em 0
