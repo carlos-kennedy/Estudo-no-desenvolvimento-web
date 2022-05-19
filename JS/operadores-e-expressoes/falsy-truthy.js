8efb8212081cfbf052b960c466009ee054708770
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-05-18 22:27:44 -0300

    main

---
+++ JS/operadores-e-expressoes/falsy-truthy.js
@@ -0,0 +1,48 @@
+// Type conversion (typecasting) vs type Coersion
+// Exemplo:
+// console.log(Number('9') + 50)
+
+
+
+// FALSY
+// Quando um valor é considerado false em contextos onde um booleano é obrigatório (condicionais e loops)
+
+// false
+// 0
+// -0
+// ""
+// null
+// undefined
+// NaN
+
+console.log(false ? 'Estou verdadeiro' : 'Estou falso')
+console.log(0 ? 'Estou verdadeiro' : 'Estou falso')
+console.log(- 0 ? 'Estou verdadeiro' : 'Estou falso')
+console.log("" ? 'Estou verdadeiro' : 'Estou falso')
+console.log(null ? 'Estou verdadeiro' : 'Estou falso')
+console.log(undefined ? 'Estou verdadeiro' : 'Estou falso')
+console.log(NaN ? 'Estou verdadeiro' : 'Estou falso')
+
+// Truthy
+// Quando um valor é considerado true em contextos onde um booleano é obrigatório (condicionai e loops)
+
+// true
+// {}
+// []
+// 1
+// 3.23
+// "0"
+// "false"
+// -1
+// Infinity
+// -Infinity
+
+console.log(true ? 'Estou verdadeiro' : 'Estou Falso')
+console.log({} ? 'Estou verdadeiro' : 'Estou Falso')
+console.log([] ? 'Estou verdadeiro' : 'Estou Falso')
+console.log(1 ? 'Estou verdadeiro' : 'Estou Falso')
+console.log(3.23 ? 'Estou verdadeiro' : 'Estou Falso')
+console.log("0" ? 'Estou verdadeiro' : 'Estou Falso')
+console.log("false" ? 'Estou verdadeiro' : 'Estou Falso')
+console.log(Infinity ? 'Estou verdadeiro' : 'Estou Falso')
+console.log(-Infinity ? 'Estou verdadeiro' : 'Estou Falso')
---
+++ JS/operadores-e-expressoes/operador-de-string.js
@@ -0,0 +1,15 @@
+// Operador de String (String operator)
+
+// Comparison
+// console.log('A' == 'a')
+// console.log('B' == 'b')
+
+// Concatenation (concatenação)
+// Retorna a união de duas Strings
+console.log('A' + 'B')
+let anyWord = 'Sou'
+let otherWord = 'Uma'
+let Word = 'Palavra'
+anyWord += 'Ele'
+console.log()
+console.log(anyWord + otherWord + Word)
